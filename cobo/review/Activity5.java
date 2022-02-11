18e40b94d7a41b9aaccdaf7db7e7e70690bf926f
Andrey Sokolov <andrey.sokolov123@gmail.com>
2022-02-11 13:53:27 -0500

    renamed 010_review; also added Activity5

---
+++ cobo/review/Activity5.java
@@ -0,0 +1,24 @@
+public class Activity5 {
+
+  static char[] vowels = {'a','e','i','o','u'};
+  public static String piglatin(String word){
+    word = word.toLowerCase();
+    boolean temp = false;
+    for (int i = 0; i < vowels.length; i++){
+      if ((word.charAt(0) == vowels[i]) || (word.charAt(word.length()-1) == 'y')){
+        temp = true;
+      }
+    }
+    if (temp == true) {
+      word = word.substring(1) + (word.charAt(0)) + "yay";
+    } else{
+      word = word.substring(1) + (word.charAt(0)) + "ay";
+    }
+    return word;
+  }
+  public static void main(String[] args){
+    System.out.println(piglatin("Yay"));
+    System.out.println(piglatin("bedroom"));
+  }
+
+}
