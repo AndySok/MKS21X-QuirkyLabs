a96763844c32acb6e7f81b37dd8db9c05c007a21
Andrey Sokolov <andrey.sokolov123@gmail.com>
2022-02-14 13:50:42 -0500

    7/30 done

--- cb/recursion-1/Recursion.java
+++ cb/recursion-1/Recursion.java
@@ -21,6 +21,23 @@ public class Recursion{
     else if(bunnies % 2 == 0) return 3 + bunnyEars2(bunnies-1);
     else return 2 + bunnyEars2(bunnies-1);
   }
+  //triangle
+  public static int triangle(int rows) {
+    if(rows > 1) return rows + triangle(rows-1);
+    else return rows;
+  }
+  //sumDigits
+  public static int sumDigits(int n){
+    if(n>9) return n%10 + sumDigits(n/10);
+    else return n;
+  }
+  //count7
+  public static int count7(int n) {
+    if(n%10==7 && n>9) return 1 + count7(n/10);
+    else if(n%10==7) return 1;
+    else if(n>9) return 0 + count7(n/10);
+    else return 0;
+  }
   public static void main(String[] args){
     System.out.println(factorial(4)); //returns 24
     System.out.println(factorial(6)); //returns 720
@@ -30,5 +47,11 @@ public class Recursion{
     System.out.println(fibonacci(5)); //returns 5
     System.out.println(bunnyEars2(3)); //returns 7
     System.out.println(bunnyEars2(10)); //returns 25
+    System.out.println(triangle(1)); //returns 1
+    System.out.println(triangle(6)); //returns 21
+    System.out.println(sumDigits(126)); //returns 9
+    System.out.println(sumDigits(1111)); //returns 4
+    System.out.println(count7(7)); //returns 1
+    System.out.println(count7(99999)); //returns 0
   }
 }
