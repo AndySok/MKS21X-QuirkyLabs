7183412ade5456012673a1c7de72ab93b20882f7
Andrey Sokolov <andrey.sokolov123@gmail.com>
2022-03-01 17:10:58 -0500

    KnightTour working for boards up to 6x6; optimized method required for larger boards

--- 68_knight/KnightTour.java
+++ 68_knight/KnightTour.java
@@ -17,14 +17,21 @@
  * ALGO
  *
  * DISCO
+ * not every board is equal. as the boards get bigger, it gets a lot harder to compute a solvable set. 
+ * the moat idea is ingenious, but it's hard to grasp without actually learning about it in class
+ * 
  *
  * QCC
+ * Why does the moat size differ with the different board size? 
+ * At what point does using recursion become impractical when trying to solve Knight's Tour?
+ * What's happening with rumpus-room records??
+ * I wonder how Fred is doing... 
  *
  * Mean execution times for boards of size n*n:
- * n=5   __s    across __ executions
- * n=6   __s    across __ executions
- * n=7   __s    across __ executions
- * n=8   __s    across __ executions
+ * n=5   0.172s    across 5 executions
+ * n=6   1.553s    across 5 executions
+ * n=7   computed for more than a minute    across 1 executions
+ * n=8   computed for more than 5 minutes    across 1 executions
  *
  * POSIX PROTIP: to measure execution time from BASH, use time program:
  * $ time java KnightTour 5
@@ -52,14 +59,15 @@ public class KnightTour
     TourFinder tf = new TourFinder( n );
 
     //clear screen using ANSI control code
-    System.out.println( "[2J" );
+    System.out.println( "\u001b[2J" );
 
     //display board
     System.out.println( tf );
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for fixed starting location, use line below:
-    tf.findTour( 2, 2, 1 );
+    if(n<8) tf.findTour( 4, 4, 1 );
+    else tf.findTour( 4, 4, 1 );
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -84,20 +92,26 @@ class TourFinder
   //instance vars
   private int[][] _board;
   private int _sideLength; //board has dimensions n x n
-  private boolean _solved = ???
+  private boolean _solved = false;
 
   //constructor -- build board of size n x n
   public TourFinder( int n )
   {
-    _sideLength = ???
-
+    _sideLength = n;
+    int _moatSize = 2;
+    if(n < 8) _moatSize += 2;
     //init 2D array to represent square board with moat
-    _board =
+    _board = new int[_sideLength + _moatSize*2][_sideLength + _moatSize*2];
 
     //SETUP BOARD --  0 for unvisited cell
     //               -1 for cell in moat
     //---------------------------------------------------------
-    ???
+     for(int y=0;y<_board.length;y++){
+         for(int x=0;x<_board[y].length;x++){
+            if((y >= _moatSize && x >= _moatSize) && y< (_board.length - _moatSize) && x< (_board.length - _moatSize)) _board[y][x] = 0;
+            else _board[y][x] = -1;
+         }
+     }
     //---------------------------------------------------------
 
   }//end constructor
@@ -109,13 +123,13 @@ class TourFinder
   public String toString()
   {
     //send ANSI code "ESC[0;0H" to place cursor in upper left
-    String retStr = "[0;0H";
+    String retStr = "\u001b[0;0H";
     //emacs shortcut: C-q, then press ESC
     //emacs shortcut: M-x quoted-insert, then press ESC
 
     int i, j;
-    for( i=0; i < _sideLength+4; i++ ) {
-      for( j=0; j < _sideLength+4; j++ )
+    for( i=0; i < _board.length; i++ ) {
+      for( j=0; j < _board[i].length; j++ )
         retStr = retStr + String.format( "%3d", _board[j][i] );
       //"%3d" allots 3 spaces for each number
       retStr = retStr + "\n";
@@ -150,24 +164,24 @@ class TourFinder
     //delay(50); //slow it down enough to be followable
 
     //if a tour has been completed, stop animation
-    if ( ??? ) System.exit(0);
+    if ( _solved ) System.exit(0);
 
     //primary base case: tour completed
-    if ( ??? ) {
-      ???
+    if ( moves >= _sideLength * _sideLength && _board[y][x] == 0) {
+      _board[y][x] = moves;
+      _solved = true;
       System.out.println( this ); //refresh screen
       return;
     }
     //other base case: stepped off board or onto visited cell
-    if ( ??? ) {
+    if ( _board[y][x] != 0 ) {
       return;
     }
     //otherwise, mark current location
     //and recursively generate tour possibilities from current pos
     else {
-
       //mark current cell with current move number
-      _board[x][y] = ???
+      _board[y][x] = moves;
 
       System.out.println( this ); //refresh screen
 
@@ -182,11 +196,15 @@ class TourFinder
        *     g . . . b
        *     . h . a .
       ******************************************/
-      ???
+      int[] xOffset = {-2, -1, 1, 2, 2, 1, -1, -2};
+      int[] yOffset = {1, 2, 2, 1, -1, -2, -2, -1};
+      for(int i=0;i<8;i++){
+          findTour(x+xOffset[i], y+yOffset[i], moves+1);
+      }
 
       //If made it this far, path did not lead to tour, so back up...
       // (Overwrite number at this cell with a 0.)
-        ???
+      _board[y][x] = 0;
 
       System.out.println( this ); //refresh screen
     }
