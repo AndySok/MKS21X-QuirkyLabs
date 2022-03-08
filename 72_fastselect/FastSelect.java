0d1ef16d810d8da13179dd0a60ebce638a13d8cf
AndySok <andrey.sokolov123@gmail.com>
2022-03-08 14:29:01 -0500

    created FastSelect.java

---
+++ 72_fastselect/FastSelect.java
@@ -0,0 +1,31 @@
+// Team Fred - [FILL OUT LATER]
+// APCS pd8
+// HW72 - So So Fast
+// 2022-03-08
+// time spent: 1 hrs
+
+/**
+* ALGO
+* 
+* DISCO
+*
+* QCC
+*
+* q0: What does it do?
+*
+* q1: What is its Big-Oh runtime?
+* 
+**/
+
+public class FastSelect{
+ public static int FastSelect(){
+   //Categorize the execution time of your algorithm using Big-Oh notation.
+ }
+  
+ public static void main(String[] args){
+   //build test calls
+   
+   //Describe best- and worst-case scenarios and Big-O classification for each. 
+   //Include test calls in your main method illustrating each.
+ }
+}
