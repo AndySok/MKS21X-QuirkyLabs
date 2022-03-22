ab73291312a9baff9260fd6c2e3144167ecf7cb4
Andrey Sokolov <andrey.sokolov123@gmail.com>
2022-03-22 16:25:25 -0400

    rest of methods implemented; main() tested;

--- 81_itr/ItrWork.java
+++ 81_itr/ItrWork.java
@@ -8,13 +8,14 @@
 
 /*
  * DISCO
- * -
- * -
- * -
+ * - Fairly straightforward tool to peruse
+ * - This is just like a Scanner!
+ * - Interesting how you can use an interface to do most things in methods
+ *
  * QCC
- * -
- * -
- * -
+ * - What other methods does the Iterator class have?
+ * - How come you can initialize Iterator using LIST_INSTANCE.iterator()?
+ * - What's the big difference between using Iterator and a FOREACH loop?
  */
 
 /***
@@ -44,7 +45,7 @@ public class ItrWork
   public static boolean foundB( Integer key,
                                 List<Integer> L )
   {
-    Iterator it = L.iterator();
+    Iterator<Integer> it = L.iterator();
     while(it.hasNext()){
       if(it.next() == key) return true;
     } return false;
@@ -54,14 +55,23 @@ public class ItrWork
   //returns a list containing the odd numbers in L
   public static List<Integer> oddsA( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    List<Integer> odds = new ArrayList<Integer>();
+    for(int i=0; i<L.size(); i++){
+      if(L.get(i) % 2 == 1) odds.add(L.get(i));
+    }
+    return odds;
   }
 
   //explicitly using an iterator
   //returns a list containing the odd numbers in L
   public static List<Integer> oddsB( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    Iterator<Integer> it = L.iterator();
+    List<Integer> odds = new ArrayList<Integer>();
+    while(it.hasNext()){
+      int num = it.next();
+      if(num % 2 == 1) odds.add(num);
+    } return odds;
   }
 
 
@@ -69,16 +79,18 @@ public class ItrWork
   //modifies L s.t. it contains no evens
   public static void removeEvens( List<Integer> L )
   {
-    /*** YOUR IMPLEMENTATION HERE ***/
+    Iterator<Integer> it = L.iterator();
+    while(it.hasNext()){
+      if(it.next() % 2 == 0) it.remove();
+    }
   }
 
 
   public static void main( String [] args )
   {
-    /*~~~~~~~~~~~~~~~m~o~v~e~~m~e~~d~o~w~n~~~~~~~~~~~~~~
 
     //var type: List   obj type: ?
-    List<Integer> L =
+    List<Integer> L = new ArrayList<Integer>();
 
     for( int i = 0; i < 10; i++ )
       L.add(i);
@@ -87,10 +99,19 @@ public class ItrWork
     // TASK: write code to print the contents of L...
 
     // a) using a FOREACH loop
+    for( int i = 0; i < L.size(); i++){
+      System.out.print(i);
+      if(i < L.size() -1 ) System.out.print(", ");
+    }
 
+    System.out.print("\n \n");
 
     // b) explicitly using an iterator
-
+    Iterator<Integer> it = L.iterator();
+    while(it.hasNext()){
+      System.out.print(it.next()+" ");
+    }
+    System.out.print("\n");
 
     System.out.println("\nTesting foundA...");
     System.out.println("9 in L? -> " + foundA(9,L) );
@@ -111,6 +132,7 @@ public class ItrWork
     System.out.println("\nTesting removeEvens...");
     removeEvens(L);
     for( int n : L ) System.out.println(n);
+    /*~~~~~~~~~~~~~~~m~o~v~e~~m~e~~d~o~w~n~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
   }//end main
