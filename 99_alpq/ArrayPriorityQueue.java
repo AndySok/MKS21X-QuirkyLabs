a2d69dffb70fc6092ae9522e93b398cc4d8da0af
Andrey Sokolov <andrey.sokolov123@gmail.com>
2022-05-15 00:41:08 -0400

    priority works! now to the notes...

--- 99_alpq/ArrayPriorityQueue.java
+++ 99_alpq/ArrayPriorityQueue.java
@@ -1,48 +1,70 @@
+/**
+ * Eddie's Fanclub :: Ziying Jian, Andrey Sokolov, Joshua Gao
+ * APCS pd08
+ * HW99 -- Some Are More Equal Than Others, Codified
+ * 2022-05-13f
+ * time spent: 1 hrs
+ */
+
 import java.util.ArrayList;
 
 public class ArrayPriorityQueue implements PriorityQueue {
-    private ArrayList<Priority> Q;
+    private ArrayList<Integer> Q;
 
     public ArrayPriorityQueue(){
-        Q = new ArrayList<Priority>();
+        Q = new ArrayList<Integer>();
     }
 
-    public void add( String cargo, int priority ) {
-        for(int i = Q.size() - 1; i >= 0; i--){
-            if(Q.get(i).priority() == priority){
-                add(cargo, i+1);
-            } else if () //finish
-        }
+    public void add( int cargo ) {
+        Integer superCargo = cargo;
+        Q.add(position(cargo), cargo);
     } //O(n)
+
+    //pre-req: cargo is defined
+    public int position(Integer cargo){
+        int i = 0;
+        while (i < Q.size() && cargo.compareTo(Q.get(i)) > 0) {
+            i++;
+          }
+          return i;        
+    }
 
     public boolean isEmpty() {
         return Q.isEmpty();
     } //O(1)
 
-    public String peekMin() {
-        return Q.get( Q.size() - 1 ).cargo();
+    public int peekMin() {
+        return Q.get( Q.size() - 1 );
     } //O(1)
     
-    public String removeMin() {
-        return Q.remove( Q.size() - 1 ).cargo();
+    public int removeMin() {
+        return Q.remove( Q.size() - 1 );
     } //O(1)
-}
 
-public class Priority {
-    private String cargo;
-    private int priority;
-
-    public Priority(String cargo, int priority){
-        this.cargo = cargo;
-        this.priority = priority;
+    public String toString(){
+        return Q.toString();
     }
 
-    public String cargo(){
-        return cargo;
-    }
+    public static void main(String[] args){
+        ArrayPriorityQueue poo = new ArrayPriorityQueue();
+        poo.add(5);
+        poo.add(3);
+        poo.add(4);
+        poo.add(7);
+        poo.add(8);
+        poo.add(1);
+        System.out.println("poo: "+  poo);
 
-    public int priority(){
-        return priority;
+        System.out.println();
+        System.out.println("min: " + poo.peekMin());
+        System.out.println("removeMin: " + poo.removeMin());
+        System.out.println("min: " + poo.peekMin());
+        System.out.println("removeMin: " + poo.removeMin());
+        System.out.println("min: " + poo.peekMin());
+        System.out.println("removeMin: " + poo.removeMin());
+
+        System.out.println();
+        System.out.println("poo: "+  poo);
     }
 
 }
--- 99_alpq/PriorityQueue.java
+++ 99_alpq/PriorityQueue.java
@@ -1,3 +1,11 @@
+/**
+ * Eddie's Fanclub :: Ziying Jian, Andrey Sokolov, Joshua Gao
+ * APCS pd08
+ * HW99 -- Some Are More Equal Than Others, Codified
+ * 2022-05-13f
+ * time spent: 1 hrs
+ */
+
 public interface PriorityQueue
 {
     public void add( int x );
